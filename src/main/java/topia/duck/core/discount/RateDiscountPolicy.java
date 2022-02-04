b681f076ad7302c43f25a115ba87742a7e07156b
2giduck <cosmos7256@naver.com>
2022-02-04 23:36:17 +0900

    ENH: 새로운 할인 정책 개발

---
+++ src/main/java/topia/duck/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,16 @@
+package topia.duck.core.discount;
+
+import topia.duck.core.member.Grade;
+import topia.duck.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade()== Grade.VIP)
+            return price*discountPercent/100;
+        else
+            return 0;
+    }
+}
