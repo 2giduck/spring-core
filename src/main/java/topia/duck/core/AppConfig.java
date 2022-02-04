599e06cb1a64d5bbc0ab44a86bbe09d8c59e77be
2giduck <cosmos7256@naver.com>
2022-02-05 00:13:05 +0900

    ENH: 스프링으로 전환하기

--- src/main/java/topia/duck/core/AppConfig.java
+++ src/main/java/topia/duck/core/AppConfig.java
@@ -1,7 +1,8 @@
 package topia.duck.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import topia.duck.core.discount.DiscountPolicy;
-import topia.duck.core.discount.FixDiscountPolicy;
 import topia.duck.core.discount.RateDiscountPolicy;
 import topia.duck.core.member.MemberRepository;
 import topia.duck.core.member.MemberService;
@@ -10,19 +11,24 @@ import topia.duck.core.member.MemoryMemberRepository;
 import topia.duck.core.order.OrderService;
 import topia.duck.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository(){
+    @Bean
+    public MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
